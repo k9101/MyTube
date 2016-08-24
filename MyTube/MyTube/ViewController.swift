812cc1f5d123643357d58f83f6c22b9eec55def3
k9101 <kris9101@gmail.com>
2016-08-24 16:58:00 -0400

    Adding Xcode project

---
+++ MyTube/MyTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTube
+//
+//  Created by Kris Penney on 2016-08-24.
+//  Copyright © 2016 Kris Penney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
